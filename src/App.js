13fdbd35210c072873136c6201d0e78600cc3d97
Dan Blackmore <dan.blackmore@gmail.com>
2018-07-11 15:48:17 +0100

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import List from './components/List';
+import Form from './components/Form';
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Kanban App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="row mt-5">
+          <div className="col-md-4 offset-md-1">
+            <h2>Articles</h2>
+            <List />
+          </div>
+          <div className="col-md-4 offset-md-1">
+            <h2>Add a new article</h2>
+            <Form />
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from "./store/index";
+
+import { addArticle } from "./actions/index";
+
+window.store = store;
+window.addArticle = addArticle;
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+document.getElementById('root'));
 registerServiceWorker();
